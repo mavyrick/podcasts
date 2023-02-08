f1cb23c57d67c8bae3ac92a8726e43f9f3482adf
Josh Sorokin <jrsorokin@gmail.com>
2023-02-08 14:05:57 +0200

    Initial Commit

---
+++ podcasts/podcastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcastsApp.swift
+//  podcasts
+//
+//  Created by Josh Sorokin on 2023. 02. 08..
+//
+
+import SwiftUI
+
+@main
+struct podcastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
